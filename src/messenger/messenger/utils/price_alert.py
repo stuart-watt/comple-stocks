68cc23e5b83b08c7fc7e05c8590b541bf997ab36
stuart-watt <stuartwatt1996@gmail.com>
2023-06-15 14:16:18 +0800

    format

--- src/messenger/messenger/utils/price_alert.py
+++ src/messenger/messenger/utils/price_alert.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
+
 def create_price_alert(webhook: str, prices: pd.DataFrame):
     """Create a Discord buy alert"""
 
@@ -14,14 +15,14 @@ def create_price_alert(webhook: str, prices: pd.DataFrame):
     print("Filtering for stocks to buy or sell...")
 
     buys = prices[
-        (prices["last_close"] > 1) \
-        & (prices["last_close"] < prices["mean_close"]*0.95) \
+        (prices["last_close"] > 1)
+        & (prices["last_close"] < prices["mean_close"] * 0.95)
         & (prices["last_close"] == prices["min_close"])
     ]
 
     sales = prices[
-        (prices["last_close"] > 1) \
-        & (prices["last_close"] > prices["mean_close"]*1.05) \
+        (prices["last_close"] > 1)
+        & (prices["last_close"] > prices["mean_close"] * 1.05)
         & (prices["last_close"] == prices["max_close"])
     ]
 
@@ -36,29 +37,21 @@ def create_price_alert(webhook: str, prices: pd.DataFrame):
 
         symbols = [
             f"{name.title()} (**{symbol}**)"
-            for symbol, name in zip(
-                buys["symbol"], buys["name"]
-            )
+            for symbol, name in zip(buys["symbol"], buys["name"])
         ]
 
         embed.add_embed_field(
-            name=":arrow_up: Buy:",
-            value="\n".join(symbols),
-            inline=False,
+            name=":arrow_up: Buy:", value="\n".join(symbols), inline=False,
         )
     if len(sales) > 0:
 
         symbols = [
             f"{name.title()} (**{symbol}**)"
-            for symbol, name in zip(
-                sales["symbol"], sales["name"]
-            )
+            for symbol, name in zip(sales["symbol"], sales["name"])
         ]
 
         embed.add_embed_field(
-            name=":arrow_down: Sell:",
-            value="\n".join(symbols),
-            inline=False,
+            name=":arrow_down: Sell:", value="\n".join(symbols), inline=False,
         )
     embed.set_timestamp()
 
@@ -70,14 +63,19 @@ def create_price_alert(webhook: str, prices: pd.DataFrame):
 
     return
 
+
 def get_price_stats_for_symbol(df: pd.DataFrame) -> pd.DataFrame:
     """Get the price stats for each symbol"""
 
-    df = df.sort_values(by="timestamp")[["symbol", "close"]].groupby("symbol").agg(
-      min_close=('close', 'min'),
-      max_close=('close', 'max'),
-      mean_close=('close', 'mean'),
-      last_close=("close", "last")
+    df = (
+        df.sort_values(by="timestamp")[["symbol", "close"]]
+        .groupby("symbol")
+        .agg(
+            min_close=("close", "min"),
+            max_close=("close", "max"),
+            mean_close=("close", "mean"),
+            last_close=("close", "last"),
+        )
     )
 
     return df.reset_index()
@@ -90,5 +88,5 @@ def get_listed_companies():
         query="SELECT * FROM `stocks.listed_companies`",
         project_id="comple-389703",
         dialect="standard",
-        use_bqstorage_api=True
+        use_bqstorage_api=True,
     )
